f014b251f9654b1a5ca65b86df366db454ec9534
Nathan Cunha <nathanmartins@outlook.com>
2026-03-20 14:35:44 -0300

    feat: add hyprpaper and matugen configs

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,19 +1,24 @@
 { ... }:
 
 {
-  # Symlink all hypr ecosystem configs to their expected locations
   home.file = {
-    ".config/hypr/hyprland.conf".source = ../files/hypr/hyprland.conf;
-    ".config/hypr/colors.conf".source   = ../files/hypr/colors.conf;
-    ".config/hypr/hypridle.conf".source = ../files/hypr/hypridle.conf;
-    ".config/hypr/hyprlock.conf".source = ../files/hypr/hyprlock.conf;
+    # Hypr configs
+    ".config/hypr/hyprland.conf".source  = ../files/hypr/hyprland.conf;
+    ".config/hypr/colors.conf".source    = ../files/hypr/colors.conf;
+    ".config/hypr/hypridle.conf".source  = ../files/hypr/hypridle.conf;
+    ".config/hypr/hyprlock.conf".source  = ../files/hypr/hyprlock.conf;
     ".config/hypr/hyprpaper.conf".source = ../files/hypr/hyprpaper.conf;
-    ".config/hypr/rules.conf".source    = ../files/hypr/rules.conf;
+    ".config/hypr/rules.conf".source     = ../files/hypr/rules.conf;
 
     # Hyprpanel
     ".config/hyprpanel/config.json".source = ../files/hyprpanel/config.json;
 
     # Walker
     ".config/walker/config.toml".source = ../files/walker/config.toml;
+
+    # Matugen
+    ".config/matugen/config.toml".source = ../files/matugen/config.toml;
+    ".config/matugen/templates/hyprland-colors.conf".source = ../files/matugen/templates/hyprland-colors.conf;
+    ".config/matugen/templates/wofi-style.css".source       = ../files/matugen/templates/wofi-style.css;
   };
 }
