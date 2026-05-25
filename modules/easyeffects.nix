6f0d78b0109de64fbf4a78e03129b6ba327dea73
Nathan Cunha <nathanmartins@outlook.com>
2026-05-25 15:18:02 -0300

    modules: use home-manager easyeffects service
    Replaces manual systemd unit with built-in services.easyeffects.enable.

--- modules/easyeffects.nix
+++ modules/easyeffects.nix
@@ -1,15 +1,5 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  systemd.user.services.easyeffects = {
-    Unit = {
-      Description = "EasyEffects Audio Processing";
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      ExecStart = "${pkgs.easyeffects}/bin/easyeffects --gapplication-service";
-      Restart = "on-failure";
-      RestartSec = "2";
-    };
-  };
+  services.easyeffects.enable = true;
 }
